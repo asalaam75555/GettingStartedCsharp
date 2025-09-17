0fcf14ccb6a041222f9574f46cd7ebd51c9b640a
Abdul Salaam <asalaam75555@gmail.com>
2025-09-17 20:04:25 +0530

    add finshed creating creating classes chapter

--- IntroductionToOop/CreatingClasses.cs
+++ IntroductionToOop/CreatingClasses.cs
@@ -13,14 +13,24 @@
         // we can create an object from a class by using the new keyword
         OurClass ourObject = new OurClass();
 
+        MyClass myClass = new MyClass();
+
         // we can use the short-form method to create an object
         OurClass ourObject2 = new();
+
+        MySecondClass mySecondClass = new();
 
         // let's make a new instance!
         OurSecondClass ourSecondObject = new();
         ourSecondObject.ExampleMethod();
 
         int result = ourSecondObject.ExampleFunction();
+
+
+        MySecondClass mySecondClass1 = new();
+        mySecondClass1.SomeMethod();
+
+        int returnedValue = mySecondClass1.someFunction();
 
         // where else have we seen classes and objects?
         // the Console class!
@@ -29,6 +39,9 @@
         // Console.WriteLine looks a little bit different...
         // why didn't we have to create a new Console object?
         // we'll see why in a later lesson!
+
+        Console.WriteLine("why have we not created a object for this console class");
+        Console.WriteLine($"the returned value of the examplefuction class created is {returnedValue}");
     }
 
     // in C# we can create a class by using the class keyword
@@ -50,6 +63,24 @@
         public int ExampleFunction()
         {
             return 42;
+        }
+    }
+
+    class MyClass
+    {
+
+    }
+
+    public class MySecondClass
+    {
+        public void SomeMethod()
+        {
+            Console.WriteLine("this is some example method");
+        }
+
+        public int someFunction()
+        {
+            return 22;
         }
     }
 }
--- IntroductionToOop/Program.cs
+++ IntroductionToOop/Program.cs
@@ -1,4 +1,7 @@
 ﻿Console.WriteLine("Pick an example class to run!");
 
-Constructors constructors = new Constructors();
-constructors.RunExample();
+//Constructors constructors = new Constructors();
+//constructors.RunExample();
+
+CreatingClasses creatingClasses = new CreatingClasses();
+creatingClasses.RunExample();
